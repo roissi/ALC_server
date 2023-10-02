1970535106d15372eee69da9f2885e8364b856f7
roissi <cyrildegraeve@gmail.com>
2023-10-02 15:24:23 +0200

    cards suggestion

--- controllers/suggestionController.js
+++ controllers/suggestionController.js
@@ -46,6 +46,23 @@ export const createSuggestion = async (req, res) => {
     }
 };
 
+export const markSuggestionAsAddedToAgenda = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const [updated] = await GPTSuggestion.update({ is_added_to_agenda: true }, {
+            where: { id: id }
+        });
+        if (updated) {
+            const markSuggestionAsAddedToAgenda = await GPTSuggestion.findOne({ where: { id: id } });
+            return res.status(200).json({ suggestion: markSuggestionAsAddedToAgenda });
+        }
+        throw new ValidationError('Suggestion non trouvée');
+    } catch (error) {
+        console.error('Erreur lors du marquage de la suggestion:', error);
+        res.status(500).json({ error: 'Erreur du serveur' });
+    }
+};
+
 export const updateSuggestion = async (req, res) => {
     try {
         const { id } = req.params;
