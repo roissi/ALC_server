24c8a62293c33758cd3f45e2fc995e48d6913f3f
roissi <cyrildegraeve@gmail.com>
2023-10-20 23:51:16 +0200

    pool added

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,28 +1,46 @@
 export default {
-    development: {
-      username: process.env.DB_USER || 'roissi',
-      password: null,
-      database: process.env.DB_NAME || 'alcai',
-      host: 'localhost',
-      port: 5432,
-      dialect: 'postgres'
-    },
-    test: {
-      username: process.env.DB_USER || 'roissi',
-      password: null,
-      database: process.env.DB_NAME || 'alcai',
-      host: 'localhost',
-      port: 5432,
-      dialect: 'postgres'
-    },
-    production: {
-      url: process.env.DATABASE_URL,
-      username: process.env.DB_USER || 'defaultUser',
-      password: null,
-      database: process.env.DB_NAME || 'defaultDb',
-      host: 'localhost',
-      port: 5432,
-      dialect: 'postgres',
-      logging: false
+  development: {
+    username: process.env.DB_USER || 'roissi',
+    password: null,
+    database: process.env.DB_NAME || 'alcai',
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+      acquire: 30000
     }
-  };
+  },
+  test: {
+    username: process.env.DB_USER || 'roissi',
+    password: null,
+    database: process.env.DB_NAME || 'alcai',
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+      acquire: 30000
+    }
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    username: process.env.DB_USER || 'defaultUser',
+    password: null,
+    database: process.env.DB_NAME || 'defaultDb',
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5, // Vous pouvez ajuster cette valeur en fonction des limitations de votre hébergeur de base de données
+      min: 0,
+      idle: 10000,
+      acquire: 30000
+    }
+  }
+};
